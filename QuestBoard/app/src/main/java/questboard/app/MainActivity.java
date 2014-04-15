67c33f9cae270505fe74b151a82f7ebc0b79bac1
Fedoraman42 <neokirby15@yahoo.com>
2014-04-14 21:00:02 -0400

    Getting MainActivity and LoginActivity set up appropriately.  All other content either has a placeholder, or will soon.
    Currently having an issue with adding the provided login credentials, but I have a couple more ideas to try to make that work.

--- QuestBoard/app/src/main/java/questboard/app/MainActivity.java
+++ QuestBoard/app/src/main/java/questboard/app/MainActivity.java
@@ -1,38 +1,72 @@
 package questboard.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity
+public class MainActivity extends Activity
 {
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(android.R.layout.activity_main);
-    }
+	static final int LOGIN_REQUEST = 1;
+	static final int LOGGED_IN = 0;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		//call LoginActivity, wait for successful login
+		Intent loginIntent = new Intent(this, LoginActivity.class);
+		startActivityForResult(loginIntent, LOGIN_REQUEST);
+
+		//handle result from the login activity
+		this.onActivityResult(LOGIN_REQUEST, LOGGED_IN, loginIntent);//is this right?
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(android.R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings)
-            return true;
-        return super.onOptionsItemSelected(item);
-    }
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings)
+			return true;
+		return super.onOptionsItemSelected(item);
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		if(requestCode == LOGIN_REQUEST)
+		{
+			if(resultCode == 1)//user logged in as a quester
+			{
+				//finish
+			}
+			else if(resultCode == 2)//user logged in as a quest giver
+			{
+				//finish
+			}
+			else//something messed up, show an error and end the program
+			{
+				//finish
+			}
+		}
+		else//something messed up, show an error and end the program
+		{
+			//finish
+		}
+	}
 }
